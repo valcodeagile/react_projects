b683b8bc0a1556f821d6d32c82476d3fba296ebb
Valentyn <valentyn.sopilkov@drtmoldes.local>
2020-01-13 10:58:14 +0000

    Initial commit

---
+++ myproject/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, world!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}><Text style={styles.boldText}></Text>Hello, world!</Text>
+        <Text>Hello, world!</Text>
+        <Text>Hello, world!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body:{
+    backgroundColor: 'yellow',
+    padding: 40,
+    fontWeight: 'bold'
+  },
+});
