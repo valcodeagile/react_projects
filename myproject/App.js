f1484486eb2479d8afdde3da1f1d2ed778d7428e
Valentyn <valentyn.sopilkov@drtmoldes.local>
2020-01-13 11:58:16 +0000

    lesson 6

--- myproject/App.js
+++ myproject/App.js
@@ -1,43 +1,43 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('shaun');
-  const [age, setAge] = useState(30);
-
+  const [people, setPeople] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' }, 
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-          multiline
-          style={styles.input}
-          placeholder='e.g. John Doe'
-          onChangeText={(val) => {setName(val)}}
-          />
-      <TextInput 
-          keyboardType='numeric'
-          style={styles.input}
-          placeholder='e.g. 99'
-          onChangeText={(val) => {setAge(val)}}
-          />
-        <Text >name: {name}, age: {age}</Text>
+      <ScrollView>
+        {people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          ))}
+      </ScrollView> 
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  input:{
-    borderWidth:1,
-    borderColor:'#777',
-    padding:8,
-    margin:10,
-    width: 200
+  item: {
+    // flex: 1,
+    backgroundColor: 'pink',
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding:30,
+    fontSize: 24
   },
 });
