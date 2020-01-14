9664933379558f4759d3d7c81e11a00b2e0e2419
Valentyn <valentyn.sopilkov@drtmoldes.local>
2020-01-14 16:28:26 +0000

    lesson 21

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { globalStyles } from '../styles/global';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
-export default function Home(){
+export default function Home({navigation}){
+
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+        // navigation.push('ReviewDetails');
+    }
     return (
         <View style={styles.container}>
             <Text style={styles.titleText}>Home Screen</Text>
+            <Button title='go to review dets' onPress={pressHandler}/>
         </View>
     );
 }
--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import { gStyles } from '../styles/global';
 
-export default function ReviewDetails(){
+export default function ReviewDetails({navigation}){
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={gStyles.container}>
             <Text>RevieDetails Screen</Text>
+            <Button title="back to home screen" onPress={pressHandler}/>
         </View>
     );
 }
