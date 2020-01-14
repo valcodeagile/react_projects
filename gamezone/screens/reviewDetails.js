492e5cc6b2e3bb1041d9c73cf3077858521dbaad
Valentyn <valentyn.sopilkov@drtmoldes.local>
2020-01-14 09:58:57 +0000

    Lesson 16

---
+++ gamezone/screens/reviewDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default function RevieDetails(){
+    return (
+        <View style={styles.container}>
+            <Text>RevieDetails Screen</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24,
+    }
+});
